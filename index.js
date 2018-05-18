b832b2e27c2d56bc852bad128193fde0b21d90af
Kyle Spencer <spencerk2@vcu.edu>
2018-05-18 21:34:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 function shout(a) {return a.toUpperCase()}
 function shout(a) {return a.toLowerCase()}
+function logShout(a) {return console.log(a.toUpperCase())}
+function logShout(a) {return console.log(a.toLowerCase())}
