4a8086619b30ddbda56fd8c525f418f9d478e6f7
Kyle Spencer <spencerk2@vcu.edu>
2018-05-18 21:48:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function whisper(a) {return a.toLowerCase()}
 function logShout(a) {return console.log(a.toUpperCase())}
 function logWhisper(a) {return console.log(a.toLowerCase())}
 function sayHiToGrandma(a) {
-  if (a === a.toLowerCase()) return `I can\'t hear you!`
+  if (a === a.toLowerCase()) {return `I can\'t hear you!`}
   }
 else if (a === a.toUpperCase()) {return `YES INDEED!`
 }
